e77b3fce5bcec6f30d5f922e22e0e6098a3625c0
Gabriel-WH <gabrielwhartmann@gmail.com>
2022-05-18 11:26:39 -0300

    1a versao

--- script_diagrama.R
+++ script_diagrama.R
@@ -13,41 +13,44 @@ write_lines(
 )
 
 ex_df <- import(
-  file = ex_url,
+  file = ex_arq,
   format = "fwf",
-  header = TRUE,
-  #col.names = c(
-  #  "PRES(hPa)",
-  #  "HGHT(m)",
-  #  "TEMP(C)",
-  #  "DWPT()",
-  #  "RELH()",
-  #  "MIXR()",
-  #  "DRCT()",
-  #  "SKNT()",
-  #  "THTA()",
-  #  "THTE()",
-  #  "THTV()"
-  #),
+  header = FALSE,
+  col.names = c(
+    "PRES_hPa",
+    "HGHT_m",
+    "TEMP_C",
+    "DWPT",
+    "RELH",
+    "MIXR",
+    "DRCT",
+    "SKNT",
+    "THTA",
+    "THTE",
+    "THTV"
+  ),
   nrows = 82,
-  skip = 2,
-  widths = rep(7, 11),
-  #fill = TRUE
+  skip = 9,
+  widths = rep(7, 11)
 )
+
+temp <- ex_df[,3]
+press <- ex_df[,1]
+alti <- ex_df[,2]
 
 ytick <- (c(1000, 925, 850, 700, 500, 400, 300, 250, 200,
             150, 100, 50, 20, 10))
 
 pdf(file = "tentativa_diagramastuve.pdf", width = 8, height = 14)
 
-plot(x = ex_df[,3], y = ex_df[,1], col = 1, lwd = 2, type = "l", log = "y", ylab = "Pressão Atmosférica (hPa)",
-     xlab = "Temperatura do Ar (ºC)" , ylim = rev(range(ex_df[,1])+c(-10,+10)), yaxt = "n")
+plot(x = temp, y = press, col = 1, lwd = 2, type = "l", log = "y", ylab = "Pressão Atmosférica (hPa)",
+     xlab = "Temperatura do Ar (ºC)" , ylim = rev(range(press)+c(-10,+10)), yaxt = "n")
 
 axis(side=2, at=ytick, labels = FALSE)
 text(par("usr")[1], ytick,  
      labels = ytick, srt = 45, pos = 2, xpd = TRUE)
 
-lines(x = 40-ex_df[,2]*0.0065, y = ex_df[,1], lwd = 2, type = "l", ylim = rev(range(ex_df[,1])), log = "y", lty = "dashed",
+lines(x = 40-alti*0.0065, y = press, lwd = 2, type = "l", ylim = rev(range(press)), log = "y", lty = "dashed",
       col = "gray")
 
 dev.off()
